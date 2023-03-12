ca16ea658f865bb0ceccbc9dc49482a8f062c47c
claytonfortunato <claytonreis10@gmail.com>
2023-03-12 15:59:19 -0300

    feat: reset global

--- src/components/BigPlayer/styles.ts
+++ src/components/BigPlayer/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 22.356rem;
   height: 16.667rem;
-  background: purple;
+  background: #2a2141;
   border-radius: 0.6rem;
   padding: 1.8rem;
 `;
